bd4f5479e30b1c7357a13ce54bb134cbc8bec251
杭 <1942499981@qq.com>
2022-10-11 21:30:06 +0800

    first

---
+++ src/views/about.vue
@@ -0,0 +1,27 @@
+<!--  -->
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "about",
+  components: {},
+
+  data() {
+    return {};
+  },
+
+  computed: {},
+
+  watch: {},
+
+  // 生命周期 - 创建完成（可以访问当前this实例）
+  created() {},
+  // 生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {},
+
+  methods: {},
+};
+</script>
+<style lang="scss" scoped></style>
